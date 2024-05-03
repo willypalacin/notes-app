162a5cfd314c1f8ce88f857e93ac83a8207afb50
Guillermo <gpalacin@student.ie.edu>
2024-05-03 15:23:06 +0000

    public version of langserve services

---
+++ expand-context/app/server.py
@@ -0,0 +1,103 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+import vertexai
+from typing import Any
+from vertexai.preview import reasoning_engines
+from langchain_google_vertexai import VertexAI
+from langchain_community.tools import GooglePlacesTool, tool
+from langchain.agents import Tool
+from langchain_community.utilities import GoogleSearchAPIWrapper
+from langchain_google_vertexai import VertexAI, HarmBlockThreshold, HarmCategory
+from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain_google_vertexai import ChatVertexAI
+from langchain.pydantic_v1 import BaseModel
+from langchain.tools import tool
+from langchain import hub
+import requests
+import os
+
+app = FastAPI()
+
+LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+GPLACES_API_KEY = os.getenv("GPLACES_API_KEY")
+GOOGLE_CSE_ID = os.getenv("GOOGLE_CSE_ID")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+PROJECT_ID = os.getenv("PROJECT_ID")
+REGION = os.getenv("REGION")
+BUCKET = os.getenv("BUCKET")
+
+required_env_vars = ["LANGCHAIN_TRACING_V2", "LANGCHAIN_API_KEY", "GPLACES_API_KEY", "GOOGLE_CSE_ID", "GOOGLE_API_KEY", "BUCKET", "REGION", "PROJECT_ID"]
+for var in required_env_vars:
+    if os.getenv(var, None) is None:
+        raise ValueError(f"Environment variable {var} is not set.")
+
+
+class Input(BaseModel):
+    input: str  
+class Output(BaseModel):
+    output: Any
+vertexai.init(
+    project=PROJECT_ID,
+    location=REGION,
+    staging_bucket="gs://{}".format(BUCKET),
+)
+
+
+model_name = "gemini-1.5-pro-preview-0409"
+
+search = GoogleSearchAPIWrapper()
+
+search_tool = Tool(
+    name="google_search",
+    description="Search in Google for up to date information, examples: information about characters, news about sports, TV shows",
+    func=search.run,
+)
+
+places = GooglePlacesTool()
+
+places_tool = Tool(
+    name="search_places",
+    description="Search for information (address, phone numbers, opinions, details) of places like hairdresser, restaurants, monuments and their data using the Google Maps Places API",
+    func=places.run,
+)
+
+@tool("summary-tool")
+def summary_tool(url: str) -> str:
+    """Use this tool when asked for summaries or content of URLs. It gets the content of URL to be further actioned by LLM
+    Args: url
+    """
+    import requests
+    r = requests.get(url)
+    return {"content": r.content, "link": url}
+
+
+safety_settings = {
+    HarmCategory.HARM_CATEGORY_UNSPECIFIED: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+}
+
+llm = ChatVertexAI(model=model_name, temperature=0.9, safety_settings=safety_settings)
+
+prompt=hub.pull("gpalacin/agent-langchain-chat")
+
+tools = [places_tool, search_tool,  summary_tool]
+agent = create_tool_calling_agent(llm, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools)
+
+add_routes(
+    app,
+    agent_executor.with_types(input_type=Input, output_type=Output).with_config(
+        {"run_name": "agent"}
+    ),
+    path="/expand-context"
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ retrieve-notes/app/server.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+from langchain_google_firestore import FirestoreVectorStore
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_google_vertexai import VertexAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain import hub
+from langchain_core.runnables import RunnablePassthrough
+from pydantic import BaseModel
+from typing import Any
+import os
+
+
+project_id = os.environ['PROJECT_ID']
+collection = os.environ['COLLECTION']
+embedding_field =os.environ['EMBEDDING_FIELD']
+
+
+
+app = FastAPI()
+
+class Input(BaseModel):
+    input: str  
+    
+    def get_input_text(self):
+        return self.input
+        
+class Output(BaseModel):
+    output: Any
+
+embedding = VertexAIEmbeddings(
+    model_name="textembedding-gecko@003",
+    project=project_id,
+)
+vector_store = FirestoreVectorStore(
+    collection=collection,
+    embedding_service=embedding,
+    embedding_field=embedding_field,
+)
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def retrieve_notes_attr():
+    NUMBER_OF_RESULTS = 5
+    SEARCH_DISTANCE_THRESHOLD = 0.3
+    retriever = vector_store.as_retriever(
+        search_type="similarity",
+        search_kwargs={
+            "k": NUMBER_OF_RESULTS,
+            "search_distance": SEARCH_DISTANCE_THRESHOLD,
+        },
+        filters=None,
+    )
+
+    prompt = hub.pull("gpalacin/retrieval_bigquery_prompt")
+    llm = VertexAI(model_name="gemini-1.5-pro-preview-0409", temperature=0.2)
+    return retriever, prompt, llm
+
+retriever, prompt, llm = retrieve_notes_attr()
+
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+    
+add_routes(app, rag_chain, path="/retrieve-notes")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
